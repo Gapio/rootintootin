3748f9384b669570de9efe4dba65057fa2b9d1bb
JonathanWenner <115879625+JonathanWenner@users.noreply.github.com>
2023-02-03 22:10:42 +0100

    Added Jumping
    Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,30 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float movespeed = 10f;
+    public float jumpHeight;
 
     private float xInput;
     private float yInput;
 
-    void Awake() { 
+    void Awake() 
+    {
         rb = GetComponent<Rigidbody>();
     }
 
-// Update is called once per frame
-void Update() //Animations and inputs
+    // Update is called once per frame
+    void Update() //Animations and inputs
     {
         ProcessInputs();
     }
     private void FixedUpdate() // Movement
     {
         Move();
+        
     }
     private void ProcessInputs()
     {
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(xInput, jumpHeight, yInput) * jumpHeight);
+        }
     }
     private void Move()
     {
         rb.AddForce(new Vector3(xInput, 0f, yInput) * movespeed);
     }
+
 }
