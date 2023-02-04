b63f9d23351a310bb6f223ba7646e857367cb11d
JonathanWenner <115879625+JonathanWenner@users.noreply.github.com>
2023-02-04 15:11:01 +0100

    Dash
    Dash progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,24 @@ public class PlayerController : MonoBehaviour
     private float xInput;
     private float yInput;
 
+    [Header("References")]
+    public Transform Orientation;
+    public Transform playerCam;
+    // Private Rigidbody
+    //private PlayerMovementDashing pm;
+
+    [Header("Dashing")]
+    public float dashForce;
+    public float dashUpwardForce;
+    public float dashDuration;
+
+    [Header("Cooldown")]
+    public float dashCd;
+    public float dashCdTimer;
+
+    [Header("Input")]
+    public KeyCode dashKey = KeyCode.E;
+
     void Awake() 
     {
         rb = GetComponent<Rigidbody>();
@@ -20,6 +38,8 @@ public class PlayerController : MonoBehaviour
     void Update() //Animations and inputs
     {
         ProcessInputs();
+        if (Input.GetKeyDown(dashKey))
+            Dash();
     }
     private void FixedUpdate() // Movement
     {
@@ -34,10 +54,23 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(new Vector3(xInput, jumpHeight, yInput) * jumpHeight);
         }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            rb.AddForce(new Vector3(xInput, jumpHeight, yInput) * jumpHeight);
+        }
     }
     private void Move()
     {
         rb.AddForce(new Vector3(xInput, 0f, yInput) * movespeed);
     }
+    private void Dash()
+    {
+        Vector3 ForceforceToApply = Orientation.forward * dashForce + Orientation.up * dashUpwardForce;
+        rb.AddForce(ForceforceToApply, ForceMode.Impulse);
+        Invoke(nameof(ResetDash), dashDuration);
+    }
+    private void ResetDash()
+    {
 
+    }
 }
